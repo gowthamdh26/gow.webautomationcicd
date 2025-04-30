829436aeeca626b1e4deaffb321c29dcf22d0edb
gowthamdh <gowthamdh26@gmail.com>
2025-04-30 08:18:14 +0530

    updated standalone test

--- src/test/java/gow/automation/SubmitOrderStandalone.java
+++ src/test/java/gow/automation/SubmitOrderStandalone.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 
-
+//test to get the webhook continousintegration 
 public class SubmitOrderStandalone {
 
 	public static void main(String[] args) throws InterruptedException {
